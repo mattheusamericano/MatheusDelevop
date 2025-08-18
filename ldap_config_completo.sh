4224883c6fa1d4de7b4c58309dc8128dc891acf7
Matheus Americano <137636772+mattheusamericano@users.noreply.github.com>
2025-08-18 09:40:23 -0300

    Add files via upload

---
+++ ldap_config_completo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ==================================================================
+# Script para inclusao de servidores no LDAP
+# Automated LDAP Server Configuration Script
+# ==================================================================
+
+# [...] CONTEÚDO OMITIDO AQUI PARA ECONOMIZAR ESPAÇO
+
+# Run main function
+main "$@"
